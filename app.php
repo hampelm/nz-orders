a51d349c90834b37c3f998776afc003f6322b03f
Matt Hampel <matthew.hampel@gmail.com>
2013-08-27 20:10:57 -0700

    Working XML

--- app.php
+++ app.php
@@ -1,36 +1,68 @@
 <?php
-$writer = new XMLWriter();
-$writer->openURI('php://output');
-$writer->startDocument('1.0','UTF-8');
-$writer->setIndent(4);
-$writer->startElement('chart');
-   $writer->writeAttribute('lowerLimit', '0');
-   $writer->writeAttribute('upperLimit', '100');
-   $writer->writeAttribute('caption', 'Revenue');
-   $writer->writeAttribute('subcaption', 'US $ (1,000s)');
-   $writer->writeAttribute('numberPrefix', '$');
-   $writer->writeAttribute('numberSuffix', 'K');
-   $writer->writeAttribute('showValue', '1');
-   $writer->startElement('colorRange');
-      $writer->startElement('color');
-         $writer->writeAttribute('minValue', '0');
-         $writer->writeAttribute('maxValue', '50');
-         $writer->writeAttribute('color', 'A6A6A6');
+class Order {
+   private $debug = TRUE;
+
+   public $addr1;
+   public $addr2;
+   public $city;
+   public $email;
+   public $items = array();
+   public $number;
+   public $state;
+   public $name;
+   public $zip;
+   public $phone;
+
+   public function writeXML() {
+      $writer = new XMLWriter();
+      $writer->openURI('php://output');
+      $writer->startDocument('1.0','UTF-8');
+      $writer->setIndent(4);
+      $writer->startElement('OrderBatch');
+         $writer->writeAttribute('Version', '1.0');
+         $writer->startElement('FulfillmentOrder');
+            $writer->writeElement('CompanyName', $name);
+            $writer->writeElement('Line1', $addr1);
+            $writer->writeElement('Line2', $addr2);
+            $writer->writeElement('City', $city);
+            $writer->writeElement('StateProvidenceCode', $state);
+            $writer->writeElement('PostalZipCode', $zip);
+
+
+            foreach ($this->items as &$item) {
+               $writer->startElement('FulfillmentOrderItemDetail');
+                  $writer->writeElement('LineItem', $item->lineNumber);
+                  $writer->writeElement('ItemID', $item->itemId);
+                  $writer->writeElement('OrderQuantity', $item->quantity);
+               $writer->endElement();
+            }
+
+         $writer->endElement();
+
+         if ($debug) {
+            $writer->writeElement('TestOrderFlag', '1');
+         }
+
       $writer->endElement();
-      $writer->startElement('color');
-         $writer->writeAttribute('minValue', '50');
-         $writer->writeAttribute('maxValue', '75');
-         $writer->writeAttribute('color', 'CCCCCC');
-      $writer->endElement();
-      $writer->startElement('color');
-         $writer->writeAttribute('minValue', '75');
-         $writer->writeAttribute('maxValue', '100');
-         $writer->writeAttribute('color', 'E1E1E1');
-      $writer->endElement();
-   $writer->endElement();
-   $writer->writeElement('value','78.9');
-   $writer->writeElement('target','78.9');
-$writer->endElement();
-$writer->endDocument();
-$writer->flush();
+      $writer->endDocument();
+      $writer->flush();
+   }
+
+}
+
+class Item {
+   public $lineNumber;
+   public $itemId;
+   public $quantity;
+}
+
+// Create some test items
+$order = new Order;
+$item1 = new Item;
+$item2 = new Item;
+$items = Array($item1, $item2);
+$order->items = $items;
+$order->writeXML();
+
+
 ?>
